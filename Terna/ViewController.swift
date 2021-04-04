e4103612456cd5618d078842a037aec66a8f800d
laudino <laudino@minsait.com>
2021-04-04 23:12:00 +0200

    Initial Commit

---
+++ Terna/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Terna
+//
+//  Created by Luigi Audino on 04/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
